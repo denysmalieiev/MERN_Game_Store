bb98d1700f4980f86431acb1f9536a2484eb8022
shafiq <shafiq@gmail.com>
2024-03-24 15:33:04 +0300

    changed display games component grid to flexbox

--- src/components/reusable/displayGames.tsx
+++ src/components/reusable/displayGames.tsx
@@ -1,37 +1,36 @@
-import {Waiting_Api} from "../waitingApi"
-import Game_card from "../reusable/gameCard"
+import { Waiting_Api } from "../waitingApi";
+import Game_card from "../reusable/gameCard";
 
 type ApiData = {
-  games:{
-      title: string,
-      img: string,
-      description: string,
-      category: string,
-      cost: string,
-      trailer: string,
-      platform: string,
-      release: string,
-      publisher: string,}[]
+  games: {
+    title: string;
+    img: string;
+    description: string;
+    category: string;
+    cost: string;
+    trailer: string;
+    platform: string;
+    release: string;
+    publisher: string;
+  }[];
+};
 
-}
-
-  const  Display_games = ({games}:ApiData)=>{
-  
+const Display_games = ({ games }: ApiData) => {
   return (
-    <> 
-        <div className="overflow-scroll">
-<div className="grid grid-cols-2 md:grid-cols-3 md:gap-15 md:ml-5 gap-2  p-2   w-max md:items-center ">
-        {
-          games.length>0?games.map((item)=>{
-            return(
-   <Game_card data={item}/>
-            )
-          }):<Waiting_Api/>
-        }
+    <>
+      <div className='overflow-hidden w-screen flex items-center justify-center p-3'>
+        <div className='flex flex-row justify-between overflow-scroll'>
+          {games.length > 0 ? (
+            games.map((item) => {
+              return <Game_card data={item} />;
+            })
+          ) : (
+            <Waiting_Api />
+          )}
         </div>
-        </div>
+      </div>
     </>
-    )
-}
+  );
+};
 
-export default Display_games 
+export default Display_games;
--- src/components/reusable/gameCard.tsx
+++ src/components/reusable/gameCard.tsx
@@ -1,45 +1,54 @@
-import {useNavigate} from "react-router-dom" ;
-import {ApiData} from "../../redux/getGamesSlice";
-import {AddToCartBtn} from "./addToCartBTN"
+import { useNavigate } from "react-router-dom";
+import { ApiData } from "../../redux/getGamesSlice";
+import { AddToCartBtn } from "./addToCartBTN";
 
 type Props = {
-  data:ApiData
-} 
+  data: ApiData;
+};
 
 export type Count = {
-      title: string,
-      img: string,
-      description: string,
-      category: string,
-      cost: string,
-      trailer: string,
-      platform: string,
-      release: string,
-      publisher: string,
-      count:number
-}
+  title: string;
+  img: string;
+  description: string;
+  category: string;
+  cost: string;
+  trailer: string;
+  platform: string;
+  release: string;
+  publisher: string;
+  count: number;
+};
 
-const Game_card = ({data}:Props)=>{
-  const navigate = useNavigate()  
-  
+const Game_card = ({ data }: Props) => {
+  const navigate = useNavigate();
+
   return (
     <>
-    <div className="flex flex-col  w-fit   rounded shadow-sm bg-white justify-between md:m-5">
-    <div className="flex flex-col w-40  md:w-72 rounded  bg-white justify-between ">
-    <img src={data.img} className="w-40 md:w-[100%] md:mr-1 h-32 md:h-56 rounded-t" onClick={ ()=>{
- navigate(`/details/${data.title}`)
- }}/>
-    <div className="flex flex-col  justify-between mx-2 md:py-3">
-    <div className="font-bold text-sm text-gray-600 ">{data.title}</div>
-    <div className="text-sm text-gray-500 font-light">{data.category}</div>
-    <div className="text-sm font-bold text-gray-500">{data.cost}</div>
-    </div>
-    </div> 
-    <div className="md:hidden w-50 m-2">
-    <AddToCartBtn data={data} Class="bg-purple-500 w-[100%] p-2 text-sm rounded text-white font-bold bottom-0"/> 
-    </div>
-    </div>
+      <div className='flex flex-col  w-fit  rounded shadow-sm bg-white justify-between mx-5'>
+        <div className='flex flex-col w-40  md:w-72 rounded  bg-white justify-between '>
+          <img
+            src={data.img}
+            className='w-40 md:w-[100%] md:mr-1 h-32 md:h-56 rounded-t'
+            onClick={() => {
+              navigate(`/details/${data.title}`);
+            }}
+          />
+          <div className='flex flex-col  justify-between mx-2 md:py-3'>
+            <div className='font-bold text-sm text-gray-600 '>{data.title}</div>
+            <div className='text-sm text-gray-500 font-light'>
+              {data.category}
+            </div>
+            <div className='text-sm font-bold text-gray-500'>{data.cost}</div>
+          </div>
+        </div>
+        <div className='md:hidden w-50 m-2'>
+          <AddToCartBtn
+            data={data}
+            Class='bg-purple-500 w-[100%] p-2 text-sm rounded text-white font-bold bottom-0'
+          />
+        </div>
+      </div>
     </>
-    )
-}
-export default Game_card
+  );
+};
+export default Game_card;
